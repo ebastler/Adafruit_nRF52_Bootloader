d37d3b4f06551a27b6608e2bd6545a3a8a281a53
ebastler <moritz.plattner@gmx.net>
2023-05-04 00:37:54 +0200

    switched Osprey to 2.7V VDD

--- src/boards/osprey_rev_a/board.h
+++ src/boards/osprey_rev_a/board.h
@@ -27,7 +27,7 @@
 
 #define _PINNUM(port, pin)    ((port)*32 + (pin))
 
-#define UICR_REGOUT0_VALUE UICR_REGOUT0_VOUT_3V0
+#define UICR_REGOUT0_VALUE UICR_REGOUT0_VOUT_2V7
 
 /*------------------------------------------------------------------*/
 /* LED
