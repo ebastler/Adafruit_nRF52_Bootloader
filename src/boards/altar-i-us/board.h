b014725e86fa22ef0a28df2752b5d48e4a8a579f
ebastler <moritz.plattner@gmx.net>
2023-05-04 00:34:13 +0200

    moved to newer upstream version, split altar in US/UK, moved osprey to 3.0 V instead of 3.3V

---
+++ src/boards/altar-i-us/board.h
@@ -0,0 +1,65 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Ha Thach for Adafruit Industries
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef ALTAR_I_US_H
+#define ALTAR_I_US_H
+
+#define _PINNUM(port, pin)    ((port)*32 + (pin))
+
+/*------------------------------------------------------------------*/
+/* LED
+ *------------------------------------------------------------------*/
+#define LEDS_NUMBER         1
+#define LED_PRIMARY_PIN     _PINNUM(1, 19) // white caps lock LED double-used as bootloader LED
+#define LED_STATE_ON        1
+
+/*------------------------------------------------------------------*/
+/* BUTTON
+ *------------------------------------------------------------------*/
+#define BUTTONS_NUMBER      2
+#define BUTTON_1            _PINNUM(0, 18)  // unusable: RESET
+#define BUTTON_2            _PINNUM(0, 16)  // unconnected pin
+#define BUTTON_PULL         NRF_GPIO_PIN_PULLUP
+
+//--------------------------------------------------------------------+
+// BLE OTA
+//--------------------------------------------------------------------+
+#define BLEDIS_MANUFACTURER    "electronicmaterialsoffice"
+#define BLEDIS_MODEL           "Altar I US"
+
+//--------------------------------------------------------------------+
+// USB
+//--------------------------------------------------------------------+
+
+// Shared VID/PID with Feather nRF52840, will be disabled for building in the future
+#define USB_DESC_VID           0x239A
+#define USB_DESC_UF2_PID       0x0029
+#define USB_DESC_CDC_ONLY_PID  0x002A
+
+#define UF2_PRODUCT_NAME    "Altar I US"
+#define UF2_VOLUME_LABEL    "ALTAR_I_US"
+#define UF2_BOARD_ID        "Altar I US"
+#define UF2_INDEX_URL       "https://electronicmaterialsoffice.com/"
+
+#endif // ALTAR_I_US_H
